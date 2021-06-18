48aa8b385a93176dfadf8258467690311e97c58d
ashraf <ashrafeid@outlook.com>
2021-06-19 01:37:29 +0300

    change some project images

--- src/components/iconlist.js
+++ src/components/iconlist.js
@@ -1,7 +1,7 @@
 export const icons =[
     {id:1, link: "./assets/icons/html.svg",name:"HTML"},
     {id:2, link: "./assets/icons/css.svg" ,name:"CSS"},
-    {id:3, link: "./assets/icons/git.svg" ,name:"GIT"},
+    {id:3, link: "./assets/icons/sass.svg" ,name:"Sass"},
     {id:4, link: "./assets/icons/github.svg" ,name:"Github"},
     {id:5, link: "./assets/icons/javascript.svg" ,name:"Javascript"},
     {id:6, link: "./assets/icons/mongodb.svg" ,name:"MongoDB"},
@@ -17,9 +17,10 @@ export const icons =[
 
 export const porojects = [
     {id :1, src: "./assets/projects/megastore.jpg", link: "https://megastore-app.herokuapp.com",source: "https://github.com/ashrafcoding/mega-store" },
-    {id :1, src: "./assets/projects/sass-template.jpg", link: "https://ashrafcoding.github.io/sass-template/", source: "https://github.com/ashrafcoding/sass-template" },
-    {id :1, src: "./assets/projects/front-template.jpg", link: "https://ashrafcoding.github.io/front-template/", source: "https://github.com/ashrafcoding/front-template" },
-    {id :1, src: "./assets/projects/memeory.jpg", link: "https://ashrafcoding.github.io/memory-game/", source: "https://github.com/ashrafcoding/memory-game" },
-    {id :1, src: "./assets/projects/chat.jpg", link: "https://ashrafcoding.github.io/chat-app-css/" , source: "https://github.com/ashrafcoding/chat-app-css"},
-    {id :1, src: "./assets/projects/glass.jpg", link: "https://ashrafcoding.github.io/glass-page/", source: "https://github.com/ashrafcoding/glass-page" },
+    {id :2, src: "./assets/projects/countries.jpg", link: "https://countries-api-new.netlify.app/", source: "https://github.com/ashrafcoding/react-countries" },
+    {id :3, src: "./assets/projects/front-template.jpg", link: "https://ashrafcoding.github.io/front-template/", source: "https://github.com/ashrafcoding/front-template" },
+    {id :4, src: "./assets/projects/react-template.jpg", link: "https://react-temp-awesome.netlify.app/", source: "https://github.com/ashrafcoding/react-template" },
+    {id :5, src: "./assets/projects/chat.jpg", link: "https://ashrafcoding.github.io/chat-app-css/" , source: "https://github.com/ashrafcoding/chat-app-css"},
+    {id :6, src: "./assets/projects/memeory.jpg", link: "https://ashrafcoding.github.io/memory-game/", source: "https://github.com/ashrafcoding/memory-game" },
+
 ]
